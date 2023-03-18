e006a69a2fd43bd4e16a782d038a2fb97187e2da
Khaled <khaledabuhelal@gmail.com>
2023-03-18 17:48:58 +0300

    dk;fj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import ProfileCard from "./ProfileCard";
+import Alexa from "./images/alexa.jpg";
+import Cortana from "./images/cortana.jpg";
+import Siri from "./images/siri.jpg";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard handle="@alexa99" title="alexa" />
-      <ProfileCard handle="@Cortana" title="Cortana" />
-      <ProfileCard title="SirI" handle="@Siri" />
+      <ProfileCard img={Alexa} handle="@alexa99" title="alexa" />
+      <ProfileCard img={Cortana} handle="@Cortana" title="Cortana" />
+      <ProfileCard img={Siri} title="SirI" handle="@Siri" />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-function ProfileCard({ handle, title }) {
+function ProfileCard({ image, handle, title }) {
   return (
     <div>
+      <img src={image} />
       <div>title is {title}</div>
       <div>handle is {handle}</div>
     </div>
