b91931e70a35d2a3326bfd007f5952db6fcdf906
Khaled <khaledabuhelal@gmail.com>
2023-03-19 21:59:47 +0300

    Commit

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,7 +2,7 @@ import "./styles.css";
 function ProfileCard({ image, handle, title }) {
   return (
     <div className="ProfileCard">
-      <img src={image} className="ProfileImg" />
+      <img src={image} alt="pda logo" className="ProfileImg" />
       <div>{title}</div>
       <div>PDA: {handle}</div>
     </div>
